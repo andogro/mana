80808b901ac70f88795f4fb11a8c27df97587b85
German Velasco <germsvel@gmail.com>
2018-05-31 11:22:32 -0400

    Remove duplicate new_nonce

--- apps/ex_wire/lib/ex_wire/handshake/handshake.ex
+++ apps/ex_wire/lib/ex_wire/handshake/handshake.ex
@@ -215,10 +215,6 @@ defmodule ExWire.Handshake do
     {auth_msg, my_ephemeral_keypair, nonce}
   end
 
-  def new_nonce do
-    ExthCrypto.Math.nonce(@nonce_len)
-  end
-
   @doc """
   Builds a response for an incoming authentication message.
 
